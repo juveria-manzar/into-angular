b0e984634763657ec9bd07bfd65fe74b218f3b54
Juveria Manzar <juveriamanzar29@gmail.com>
2020-06-27 15:06:56 +0530

    component-decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template:`
+  <h1>This is the markup for the App Component</h1>
+  <p>this is para</p>
+  `,
+  styles:[
+    `p{
+      color:red;
+    }`
+  ],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
